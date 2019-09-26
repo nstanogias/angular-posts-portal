c86f3979038443a18e2f4bd9f488edbb5eae4e77
Nikos Stanogias <nstanogias@gmail.com>
2019-09-26 21:58:55 +0200

    deploy

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ mongoose.connect(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join(__dirname, "backend/images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/", express.static(path.join(__dirname, "angular")));
 
 // app.use((req, res, next) => {
