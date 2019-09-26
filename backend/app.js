24137900efd538bcdb9ecf24f95d34d6f5f10286
Nikos Stanogias <nstanogias@gmail.com>
2019-09-26 21:11:17 +0200

    prepare for deploy

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join(__dirname, "backend/images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/", express.static(path.join(__dirname, "angular")));
 
 // app.use((req, res, next) => {
